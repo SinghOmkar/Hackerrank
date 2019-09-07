42a7c6cee6bf168ee130a879f0f1d9e62a1c3b90
Omkar Singh <osingh2016@gmail.com>
2019-09-07 22:11:44 +0530

    A very big sum solution

---
+++ a_very_big_sum/a_very_big_sum.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * ----------------A Very Big Sum------------------
+ * https://www.hackerrank.com/challenges/a-very-big-sum/problem
+ * 
+ * Calculate and print the sum of the elements in an array,
+ * keeping in mind that some of those integers may be quite large.
+ */
+
+function aVeryBigSum($ar) {
+
+    return array_sum($ar);
+
+}
