f991ba5e776563c686094167c519d66d12dc8a6a
Omkar Singh <osingh2016@gmail.com>
2019-09-14 17:22:52 +0530

    sql advanced select

---
+++ SQL/Advanced Select/quries.sql
@@ -0,0 +1,77 @@
+/**********************************************************************************
+ *-----------------------------Type of Triangle-----------------------------------*
+ * Write a query identifying the type of each record in the TRIANGLES table using *
+ * its three side lengths. Output one of the following statements for each record *
+ * in the table:                                                                  *
+ * Equilateral: It's a triangle with 3 sides of equal length.                     *
+ * Isosceles: It's a triangle with 2 sides of equal length.                       *
+ * Scalene: It's a triangle with 3 sides of differing lengths.                    *
+ * Not A Triangle: The given values of A, B, and C don't form a triangle.         *
+ **********************************************************************************/
+ 
+SELECT
+     CASE
+         WHEN (A + B) <= C OR (A + C) <= B OR (B + C) <= A THEN "Not A Triangle"
+         WHEN A = B AND A = C THEN "Equilateral"
+         WHEN A = B OR B = C OR C = A  THEN "Isosceles"
+         WHEN A != B AND A != C AND B != C THEN "Scalene"
+    END AS triangle
+FROM triangles;
+
+
+/*********************************************************************************
+*--------------------------------The PADS----------------------------------------*
+* Generate the following two result sets:                                        *
+* Query an alphabetically ordered list of all names in OCCUPATIONS, immediately  *
+* followed by the first letter of each profession as a parenthetical (i.e.:      *
+* enclosed in parentheses). For example: AnActorName(A), ADoctorName(D),         *
+* AProfessorName(P), and ASingerName(S).                                         *
+* Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the     *
+* occurrences in ascending order, and output them in the following format:       * 
+* There are a total of [occupation_count] [occupation]s.                         *
+**********************************************************************************/
+SELECT
+    CONCAT(
+        name,
+        '(',
+        LEFT(occupation, 1),
+        ')'
+    )
+FROM occupations ORDER BY name ASC;
+
+SELECT
+    CONCAT(
+        'There are a total of ',
+        count(*),
+        ' ',
+        LOWER(occupation),
+        's.'
+    ) AS total
+FROM occupations
+GROUP BY occupation
+ORDER BY total ASC, occupation ASC;
+
+
+/***********************************************************************************
+ *_________________________Binary Tree Nodes_______________________________________*
+ * Write a query to find the node type of Binary Tree ordered by the value of the  *
+ * node. Output one of the following for each node:                                *
+ * Root: If node is root node.                                                     *
+ * Leaf: If node is leaf node.                                                     *
+ * Inner: If node is neither root nor leaf node.                                   *
+************************************************************************************/
+SELECT
+    DISTINCT n,
+    IF(
+        p IS NULL,
+        'Root',
+        IF(
+            n NOT IN (
+                SELECT DISTINCT p FROM bst WHERE p != (
+                SELECT n FROM bst WHERE p IS NULL
+            )),
+            'Leaf',
+            'Inner'
+        )
+    )
+FROM bst ORDER BY n;
