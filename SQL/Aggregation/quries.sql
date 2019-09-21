81718e89fb0d894c7e11b6e4c014683ee121b9bb
Omkar Singh <osingh2016@gmail.com>
2019-09-21 23:52:00 +0530

    weather observation station 17 solution

--- SQL/Aggregation/quries.sql
+++ SQL/Aggregation/quries.sql
@@ -163,3 +163,24 @@ FROM
 WHERE
     lat_n > 38.7780;
 
+
+/***********************************************************************************
+ *__________________________Weather Observation Station 17_________________________*
+ * https://www.hackerrank.com/challenges/weather-observation-station-17/problem    *
+ * Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N)*
+ * in STATION is greater than 38.7780. Round your answer to 4 decimal places.      *
+ ***********************************************************************************/
+SELECT
+    ROUND(long_w, 4)
+FROM
+    station
+WHERE
+    lat_n = (
+        SELECT
+            MIN(lat_n)
+        FROM
+            station
+        WHERE
+            lat_n > 38.7780
+    );
+
