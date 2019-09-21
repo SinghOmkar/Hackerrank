c35fe31d08e95946be6f5541764d381f86416b05
Omkar Singh <osingh2016@gmail.com>
2019-09-21 23:53:26 +0530

    weather observation station 19 solution

--- SQL/Aggregation/quries.sql
+++ SQL/Aggregation/quries.sql
@@ -212,3 +212,30 @@ SELECT
 FROM
     station;
 
+
+/***********************************************************************************
+ *________________________Weather Observation Station 19___________________________*
+ * https://www.hackerrank.com/challenges/weather-observation-station-19/problem    *
+ *---------------------------------------------------------------------------------*
+ * Consider P1(a, c) and P2(b, d) to be two points on a 2D plane where (a, b) are  *
+ * the respective minimum and maximum values of Northern Latitude (LAT_N) and (c,d)*
+ * are the respective minimum and maximum values of Western Longitude (LONG_W) in  *
+ * STATION.                                                                        *
+ * Query the Euclidean Distance between points P1 and P2 and format your answer to *
+ * display 4 decimal digits.                                                       *
+ *---------------------------------------------------------------------------------*
+ * Euclidean Distance :                                                            *
+ * https://www.cut-the-knot.org/pythagoras/DistanceFormula.shtml                   *
+ ***********************************************************************************/
+
+SELECT
+    ROUND(
+        SQRT(
+            POWER((MIN(lat_n) - MAX(lat_n)), 2)
+            +
+            POWER((MIN(long_w) - MAX(long_w)), 2)
+        ),
+        4
+    )
+FROM
+    station;
