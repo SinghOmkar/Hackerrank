edcfe59c0ef544d07b938da68516e644a9f05d06
Omkar Singh <osingh2016@gmail.com>
2019-09-21 23:46:26 +0530

    Top Earners problem solution

--- SQL/Aggregation/quries.sql
+++ SQL/Aggregation/quries.sql
@@ -57,3 +57,22 @@ SELECT
         (AVG(salary)) - (AVG(REPLACE(salary, '0', '')))
     ) AS avg_salary
 FROM employees;
+
+
+/***********************************************************************************
+ *_______________________________Top Earners_______________________________________*
+ * We define an employee's total earnings to be their monthly "salary x months"    *
+ * worked, and the maximum total earnings to be the maximum total earnings for any *
+ * employee in the Employee table. Write a query to find the maximum total earnings*
+ * for all employees as well as the total number of employees who have maximum     *
+ * total earnings. Then print these values as 2 space-separated integers.          *
+ *_________________________________________________________________________________*
+ * GROUP BY 1 : means group by first column in selection, eg. earnings             *
+ * ORDER BY 1  DESC : means order by first column in selection in desending order,
+ * eg. earnings.
+ ***********************************************************************************/
+
+SELECT
+    (months * salary) AS earnings,
+    COUNT(employee_id)
+FROM employee GROUP BY 1 ORDER BY 1 DESC limit 1;
