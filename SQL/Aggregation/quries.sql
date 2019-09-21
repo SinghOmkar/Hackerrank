e639c9410013fc7046d13e583ed841391ecc47d7
Omkar Singh <osingh2016@gmail.com>
2019-09-21 23:52:42 +0530

    wether observation station 18 solution

--- SQL/Aggregation/quries.sql
+++ SQL/Aggregation/quries.sql
@@ -184,3 +184,31 @@ WHERE
             lat_n > 38.7780
     );
 
+
+/***********************************************************************************
+ *__________________________Weather Observation Station 18_________________________*
+ * https://www.hackerrank.com/challenges/weather-observation-station-18/problem    *
+ *---------------------------------------------------------------------------------*
+ * Consider P1(a, b) and P2(c, d) to be two points on a 2D plane.                  *
+ * happens to equal the minimum value in Northern Latitude (LAT_N in STATION).     *
+ * happens to equal the minimum value in Western Longitude (LONG_W in STATION).    *
+ * happens to equal the maximum value in Northern Latitude (LAT_N in STATION).     *
+ * happens to equal the maximum value in Western Longitude (LONG_W in STATION).    *
+ * Query the Manhattan Distance between points P1 and P2 and round it to a scale of*
+ * 4 decimal places.                                                               *
+ *---------------------------------------------------------------------------------*
+ * Manhattan distance: https://xlinux.nist.gov/dads/HTML/manhattanDistance.html    *
+ ***********************************************************************************/
+
+SELECT
+    ROUND(
+        (
+            ABS(MIN(lat_n) - MAX(lat_n))
+            +
+            ABS(MIN(long_w) - MAX(long_w))
+        ),
+        4
+    )
+FROM
+    station;
+
