dde1e859cc256bf8b4683daebf59d5033e4ff38c
Omkar Singh <osingh2016@gmail.com>
2019-09-24 14:37:46 +0530

    Basic Join African cities

--- SQL/Basic Join/queries-joins.sql
+++ SQL/Basic Join/queries-joins.sql
@@ -20,3 +20,25 @@ INNER JOIN
             continent = 'Asia'
     ) country
 ON country.code = city.countrycode;
+
+
+/***********************************************************************************
+ *________________________________African Cities___________________________________*
+ * https://www.hackerrank.com/challenges/african-cities/problem                    *
+ *---------------------------------------------------------------------------------*
+ * Given the CITY and COUNTRY tables, query the names of all cities where the      *
+ * CONTINENT is 'Africa'.                                                          *
+ * Note: CITY.CountryCode and COUNTRY.Code are matching key columns.               *
+ ***********************************************************************************/
+SELECT 
+    city.name
+FROM
+    city,
+    (SELECT 
+        code
+    FROM
+        country
+    WHERE
+        continent = 'Africa') country
+WHERE
+    city.countrycode = country.code;
