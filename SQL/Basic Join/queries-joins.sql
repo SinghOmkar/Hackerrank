70ffbb4b8dc95f93efd8a528e4791b938f1c4fde
Omkar Singh <osingh2016@gmail.com>
2019-09-24 14:56:27 +0530

    Basic Join AVG population of continent

--- SQL/Basic Join/queries-joins.sql
+++ SQL/Basic Join/queries-joins.sql
@@ -42,3 +42,22 @@ FROM
         continent = 'Africa') country
 WHERE
     city.countrycode = country.code;
+
+
+/*************************************************************************************
+ *______________________Average Population of Each Continent_______________________  *
+ * https://www.hackerrank.com/challenges/average-population-of-each-continent/problem*
+ *-----------------------------------------------------------------------------------*
+ * Given the CITY and COUNTRY tables, query the names of all the continents          *
+ * (COUNTRY.Continent) and their respective average city populations                 * 
+ * (CITY.Population) rounded down to the nearest integer.                            *
+ * Note: CITY.CountryCode and COUNTRY.Code are matching key columns.                 *
+ *************************************************************************************/
+SELECT 
+    country.continent, FLOOR(AVG(city.population))
+FROM
+    city,
+    country
+WHERE
+    city.countrycode = country.code
+GROUP BY country.continent;
